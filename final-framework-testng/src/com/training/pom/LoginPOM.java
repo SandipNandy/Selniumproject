58d97db5624884d08be280e4202b821b1b16e19a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-19 11:01:06 +0530

    Update LoginPOM.java

--- final-framework-testng/src/com/training/pom/LoginPOM.java
+++ final-framework-testng/src/com/training/pom/LoginPOM.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class LoginPOM {
 	private WebDriver driver; 
 	
 	public LoginPOM(WebDriver driver) {
 		this.driver = driver; 
 		PageFactory.initElements(driver, this);
+		
 	}
 	
 	@FindBy(id="login")
@@ -25,11 +27,13 @@ public class LoginPOM {
 	public void sendUserName(String userName) {
 		this.userName.clear();
 		this.userName.sendKeys(userName);
+		
 	}
 	
 	public void sendPassword(String password) {
 		this.password.clear(); 
 		this.password.sendKeys(password); 
+		
 	}
 	
 	public void clickLoginBtn() {
