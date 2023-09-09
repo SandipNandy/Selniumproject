6cdd55ae2e67c120676c0151dd0ba68500bf0cc9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-09 08:24:49 +0530

    Update LoginPOM.java

--- version01/src/com/training/pom/LoginPOM.java
+++ version01/src/com/training/pom/LoginPOM.java
@@ -3,6 +3,7 @@ package com.training.pom;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
