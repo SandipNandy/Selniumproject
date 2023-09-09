146b9b836ae76783fd156af0e1d90bd81af8f02c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-09 08:23:55 +0530

    Update ScreenShot.java

--- version01/src/com/training/generics/ScreenShot.java
+++ version01/src/com/training/generics/ScreenShot.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
