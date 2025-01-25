089b0d450ed7b081d61853d958a3ed8420c92094
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2025-01-25 17:59:09 +0530

    Update AllTests.java

--- final-framework/tests/com/training/suits/AllTests.java
+++ final-framework/tests/com/training/suits/AllTests.java
@@ -1,7 +1,6 @@
 package com.training.suits;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.training.sanity.tests.LoginTest;
