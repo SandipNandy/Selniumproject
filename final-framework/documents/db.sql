7a50f296e907109e7a008b5cd21edea19cf87f15
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-13 00:02:24 +0530

    Update db.sql

--- final-framework/documents/db.sql
+++ final-framework/documents/db.sql
@@ -2,7 +2,6 @@ create table login(
 username varchar(50) not null,
 password varchar(50));
 insert into login values("admin", "admin@123"); 
-
 insert into login values("naveen", "testing@123"); 
 
 insert into login values("naveen", "testing@123"); 
