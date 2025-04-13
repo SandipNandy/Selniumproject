f43301a8abbb9b7f8429bf175dbeb029f1824126
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-13 22:22:29 +0530

    Update LoginDBTest.java

--- final-framework-testng/tests/com/training/regression/tests/LoginDBTest.java
+++ final-framework-testng/tests/com/training/regression/tests/LoginDBTest.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
