58ee51cb3294aefafd59cd536092057f302b950a
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-13 22:21:34 +0530

    Update LoginTests.java

--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -13,6 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 import org.testng.annotations.BeforeMethod;
+
 import org.testng.annotations.Test;
 
 import com.training.generics.ScreenShot;
