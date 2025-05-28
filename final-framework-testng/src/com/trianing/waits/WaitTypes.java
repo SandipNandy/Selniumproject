a47f78ce74068b9aabefe11c3137a072f08441fd
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-28 12:41:49 +0530

    Update WaitTypes.java

--- final-framework-testng/src/com/trianing/waits/WaitTypes.java
+++ final-framework-testng/src/com/trianing/waits/WaitTypes.java
@@ -3,6 +3,7 @@ package com.trianing.waits;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
