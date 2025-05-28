54efddbd99a7b0df40f115440714e37e7c9ce600
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-28 12:37:57 +0530

    Update db.sql

--- final-framework-testng/documents/db.sql
+++ final-framework-testng/documents/db.sql
@@ -9,4 +9,5 @@ insert into login values("admin", "admin@123");
 insert into login values("naveen", "testing@123");
 
 insert into login values("nav", "test@123456");
+
 insert into login values("nav1", "set@12");
