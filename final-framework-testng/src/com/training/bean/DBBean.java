dc097ecef9e282f2aa1dc4c2df9767c5d5d71464
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-28 12:40:27 +0530

    Update DBBean.java

--- final-framework-testng/src/com/training/bean/DBBean.java
+++ final-framework-testng/src/com/training/bean/DBBean.java
@@ -10,7 +10,8 @@ package com.training.bean;
 public class DBBean {
 	private String url; 
 	
-	private String driver; 
+	private String driver;
+	
 	private String userName; 
 	private String password;
 	
